91d432b8c21bb64f6422038d98cc61246e04ead2
Giang <giang@Giangs-MacBook-Pro.local>
2018-12-09 14:56:32 +0700

    1

---
+++ DetermineIfTaskIsCancel/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DetermineIfTaskIsCancel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokenSource1 = new CancellationTokenSource();
+            var token1 = tokenSource1.Token;
+            var task1 = new Task(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    token1.ThrowIfCancellationRequested();
+                    System.Console.WriteLine(i);
+                } 
+            }, token1);
+            var tokenSource2 = new CancellationTokenSource();
+            var token2 = tokenSource2.Token;
+            var task2 = new Task(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    token2.ThrowIfCancellationRequested();
+                    System.Console.WriteLine(i);
+                }
+            }, token2);
+            task1.Start();
+            task2.Start();
+            tokenSource2.Cancel();
+            System.Console.WriteLine(task1.IsCanceled);
+            System.Console.WriteLine(task2.IsCanceled);
+            System.Console.WriteLine("Main");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UseCancelWaitHandle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UseCancelWaitHandle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            var task1 = new Task(() => {
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    bool cancelled = token.WaitHandle.WaitOne(10000);
+                    System.Console.WriteLine($"{i} - {cancelled}");
+                    if (cancelled) {
+                        throw new OperationCanceledException(token);
+                    }
+                }
+            }, token);
+            task1.Start();
+            System.Console.WriteLine("Enter to cancel");
+            Console.ReadLine();
+            tokenSource.Cancel();
+            System.Console.WriteLine("Main");
+            Console.ReadKey();
+        }
+    }
+}
